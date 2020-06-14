c4e9084f8d2c2c061fb2448993c05edc6d480956
Jayson Silva <jaysonwcs@gmail.com>
2020-06-13 22:25:35 -0300

    Refactoring main program

--- Program.cs
+++ Program.cs
@@ -9,8 +9,70 @@ namespace CSharpExercicio9
     {
         static void Main(string[] args)
         {
+            Customer customer = GetCustomerData();
+
+            OrderStatus status = GetStatus();
+
+            Order order = GetOrder(status, customer);
+
+            Console.WriteLine();
+            Console.WriteLine(order.ToString());
+        }
+
+        private static Order GetOrder(OrderStatus status, Customer customer)
+        {
+            Console.Write("How many items to this order? ");
+            int itemsAmount = int.Parse(Console.ReadLine());
+
+
+            Order order = new Order(status, customer);
+
+            for (int i = 1; i <= itemsAmount; i++)
+            {
+                order.AddItem(GetItem(i));
+            }
+
+            return order;
+        }
+
+        private static OrderItem GetItem(int i)
+        {
+            Console.WriteLine($"Enter #{i} item data:");
+
+            Console.Write("Product name: ");
+            string productName = Console.ReadLine();
+
+            Console.Write("Product price: ");
+            double price = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantity: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+
+            Product product = new Product(productName, price);
+
+            return new OrderItem(quantity, product);
+        }
+
+        private static OrderStatus GetStatus()
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Enter order data:");
+            Console.WriteLine();
+            Console.WriteLine("Enter a status:");
+            Console.WriteLine("0 for PENDING_PAYMENT");
+            Console.WriteLine("1 for PROCESSING");
+            Console.WriteLine("2 for SHIPPED");
+            Console.WriteLine("3 for DELIVERED");
+            Console.Write("Type the status number here: ");
+
+            return (OrderStatus)int.Parse(Console.ReadLine());
+        }
+
+        static Customer GetCustomerData()
+        {
             Console.WriteLine("Enter customer data:");
-            
+
             Console.Write("Name: ");
             string name = Console.ReadLine();
 
@@ -20,52 +82,8 @@ namespace CSharpExercicio9
             Console.Write("Birth date (DD/MM/YYYY): ");
             DateTime birthDate = DateTime.Parse(Console.ReadLine());
 
-            Customer customer = new Customer(name, email, birthDate);
-
-            
-            
-            Console.WriteLine("Enter order data:");
-
-            Console.WriteLine("Enter a status number:");
-            Console.WriteLine("0 for PENDING_PAYMENT");
-            Console.WriteLine("1 for PROCESSING");
-            Console.WriteLine("2 for SHIPPED");
-            Console.WriteLine("3 for DELIVERED");
-
-            OrderStatus status = (OrderStatus)int.Parse(Console.ReadLine());
-
-
-            Console.Write("How many items to this order? ");
-            int itemsAmount = int.Parse(Console.ReadLine());
-
-
-            Order order = new Order(status, customer);
-
-            for (int i = 1; i <= itemsAmount; i++)
-            {
-                Console.WriteLine($"Enter #{i} item data:");
-
-                Console.Write("Product name: ");
-                string productName = Console.ReadLine();
-
-                Console.Write("Product price: ");
-                double price = double.Parse(Console.ReadLine());
-
-                Console.Write("Quantity: ");
-                int quantity = int.Parse(Console.ReadLine());
-
-
-                Product product = new Product(productName, price);
-
-                OrderItem item = new OrderItem(quantity, product);
-
-                order.AddItem(item);
-            }
-
-            Console.WriteLine();
-
-
-            Console.WriteLine(order.ToString());
+            return new Customer(name, email, birthDate);
         }
+
     }
 }
